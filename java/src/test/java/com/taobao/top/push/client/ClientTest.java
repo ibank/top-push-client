eb001a1f5e7425630fa5b75eddad06ab4624e7d9
wsky <wskyhx@gmail.com>
2013-01-07 11:15:16 +0800

    fix #5; add websocket-client source

--- java/src/test/java/com/taobao/top/push/client/ClientTest.java
+++ java/src/test/java/com/taobao/top/push/client/ClientTest.java
@@ -1,7 +1,6 @@
 package com.taobao.top.push.client;
 
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
 import java.nio.charset.Charset;
 
 import jp.a840.websocket.exception.WebSocketException;
@@ -15,20 +14,22 @@ public class ClientTest {
 	@Test
 	public void get_target_and_send_test() throws WebSocketException,
 			IOException, InterruptedException {
+		// TODO:run server for test
+
 		Client client = new Client("javaback");
 		client.connect("ws://localhost:8080/backend");
 
-		// present business layer message
 		PublishMessage pub = new PublishMessage();
 		pub.MessageId = "20130104";
 		pub.Content = "hello world!";
-
 		byte[] body = JSON.toJSONString(pub).getBytes(Charset.forName("UTF-8"));
 
+		// send message to target client
 		client.sendMessage("front", MessageType.PUBLISH, "json", body, 0,
 				body.length);
 	}
 
+	// present business layer message
 	class PublishMessage {
 		public String MessageId;
 		public String Content;
