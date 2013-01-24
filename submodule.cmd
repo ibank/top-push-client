688b18f56000e9d0d90e0c567a62a6607b4ee297
wsky <wskyhx@gmail.com>
2013-01-24 15:30:37 +0800

    update submodules

---
+++ submodule.cmd
@@ -0,0 +1,10 @@
+#git submodule add git://github.com/wsky/websocket-client.git java/external/websocket-client
+
+#git submodule add git://github.com/wsky/top-push.git java/external/top-push
+
+#git submodule add git://github.com/wsky/nmqtt.git csharp/external/nmqtt
+
+#git submodule add git://github.com/wsky/websocket-sharp.git csharp/external/websocket-sharp
+
+git submodule init
+git submodule update
